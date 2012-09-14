5db3a63c3bb5e3362c8f22646071b1f1898b8a8d
Tobias Hasslebrant <tobias.hasslebrant@gmail.com>
2012-09-14 09:42:08 +0200

    first

---
+++ PasswordProtector/MainForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace PasswordProtector
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMain = new System.Windows.Forms.RichTextBox();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtMain
+            // 
+            this.txtMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMain.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMain.Location = new System.Drawing.Point(3, 31);
+            this.txtMain.Name = "txtMain";
+            this.txtMain.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtMain.Size = new System.Drawing.Size(447, 441);
+            this.txtMain.TabIndex = 1;
+            this.txtMain.Text = "";
+            // 
+            // txtKey
+            // 
+            this.txtKey.CausesValidation = false;
+            this.txtKey.Location = new System.Drawing.Point(3, 5);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(447, 20);
+            this.txtKey.TabIndex = 0;
+            this.txtKey.UseSystemPasswordChar = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 476);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.txtMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtMain;
+        private System.Windows.Forms.TextBox txtKey;
+    }
+}
+
---
+++ PasswordProtector/MainForm.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace PasswordProtector
+{
+    public partial class MainForm : Form
+    {
+        private readonly string _fileName = GetFileName();
+        private string _loadedText = string.Empty;
+        
+        private static string GetFileName()
+        {
+            return Regex.Match(Application.ExecutablePath, @"(?i)\\([^\\.]+)\.exe$").Groups[1].ToString();
+        }
+        public MainForm()
+        {
+            InitializeComponent();
+            txtKey.KeyUp += keyUp;
+            Closing += closing;
+            txtMain.KeyDown += keyDown;
+        }
+        private void keyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode != Keys.Escape) return;
+            BeforeClose();
+            Application.Exit();
+        }
+
+        void BeforeClose()
+        {
+            if (string.IsNullOrEmpty(txtKey.Text))
+                return;
+            if (_loadedText == txtMain.Text)
+                return;
+            var dialogAnswer = MessageBox.Show("Spara?", "Spara", MessageBoxButtons.OKCancel);
+            if (dialogAnswer == DialogResult.OK)
+                SaveFile();
+        }
+
+        void closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            BeforeClose();
+        }
+
+        void keyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode != Keys.Return)
+                return;
+
+            txtKey.Enabled = false;
+            txtMain.Focus();
+            GetFileContent();
+            FormatText();
+        }
+
+        void FormatText()
+        {
+            var lines = Regex.Split(_loadedText, "\\n");
+            var index = 0;
+            foreach (var line in lines)
+            {
+                txtMain.SelectionBackColor = Color.White;
+                if(Regex.IsMatch(line,"pwd:(.+)$"))
+                    txtMain.SelectionBackColor = Color.Black;
+                txtMain.SelectedText = line;
+                if (++index != lines.Length)
+                    txtMain.SelectedText = "\n";
+            }
+        }
+
+        private void GetFileContent()
+        {
+            if(!File.Exists(_fileName))
+                return;
+
+            var encryptedText = File.ReadAllText(_fileName);
+            try
+            {
+                _loadedText = Encrypting.DecryptString(encryptedText, txtKey.Text);
+            }
+            catch (Exception)
+            {
+                System.Threading.Thread.Sleep(2000);
+                Application.Exit();
+            }
+        }
+        
+        private void SaveFile()
+        {
+            var encryptedText = string.Empty;
+            try
+            {
+                encryptedText = Encrypting.EncryptString(txtMain.Text, txtKey.Text);
+            }
+            catch (Exception)
+            {
+                Application.Exit();
+            }
+
+            File.WriteAllText(_fileName, encryptedText);
+        }
+    }
+}
